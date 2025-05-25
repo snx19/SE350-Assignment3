26cad6606f081ac231aaa5756e2c51a1dd977965
snx19 <hayato@msnx.local>
2025-05-24 23:45:08 -0500

    added Jurassic exhibit to NaturalHistoryMuseum.java

--- src/main/java/Museum/NaturalHistoryMuseum.java
+++ src/main/java/Museum/NaturalHistoryMuseum.java
@@ -4,23 +4,42 @@ import java.util.List;
 public class NaturalHistoryMuseum {
     public static void main(String[] args) {
         // set up the exhibit
-        AnimalAbstractFactory f = new CenozoicAnimalFactory();
-        System.out.println("You are in the " + f.getEra() + " exhibition");
-        System.out.println("===== " + f.getEra() + " land animals section =====");
-        List<LandAnimal> landAnimals = f.createLandAnimals();
-        for (LandAnimal a : landAnimals) {
+        AnimalAbstractFactory cenozoic = new CenozoicAnimalFactory();
+        System.out.println("You are in the " + cenozoic.getEra() + " exhibition");
+        System.out.println("===== " + cenozoic.getEra() + " land animals section =====");
+        List<LandAnimal> cenozoicLandAnimals = cenozoic.createLandAnimals();
+        for (LandAnimal a : cenozoicLandAnimals) {
             System.out.println("A " + a.getName() + " " + a.getWalking());
         }
-        System.out.println("===== " + f.getEra() + " sea animals section =====");
-        List<SeaAnimal> seaAnimals = f.createSeaAnimals();
-        for (SeaAnimal a : seaAnimals) {
+        System.out.println("===== " + cenozoic.getEra() + " sea animals section =====");
+        List<SeaAnimal> cenozoicSeaAnimals = cenozoic.createSeaAnimals();
+        for (SeaAnimal a : cenozoicSeaAnimals) {
             System.out.println("A " + a.getName() + " " + a.getSwimming());
         }
-        System.out.println("===== " + f.getEra() + " sky animals section =====");
-        List<SkyAnimal> skyAnimals = f.createSkyAnimals();
-        for (SkyAnimal a : skyAnimals) {
+        System.out.println("===== " + cenozoic.getEra() + " sky animals section =====");
+        List<SkyAnimal> cenozoicSkyAnimals = cenozoic.createSkyAnimals();
+        for (SkyAnimal a : cenozoicSkyAnimals) {
             System.out.println("A " + a.getName() + " " + a.getFlying());
         }
-        System.out.println("Thank you for visiting the " + f.getEra() + " exhibition");
+        System.out.println("Thank you for visiting the " + cenozoic.getEra() + " exhibition\n");
+
+        AnimalAbstractFactory jurassic = new JurassicAnimalFactory();
+        System.out.println("You are in the " + jurassic.getEra() + " exhibition");
+        System.out.println("===== " + jurassic.getEra() + " land animals section =====");
+        List<LandAnimal> jurassicLandAnimals = jurassic.createLandAnimals();
+        for (LandAnimal a : jurassicLandAnimals) {
+            System.out.println("A " + a.getName() + " " + a.getWalking());
+        }
+        System.out.println("===== " + jurassic.getEra() + " sea animals section =====");
+        List<SeaAnimal> jurassicSeaAnimals = jurassic.createSeaAnimals();
+        for (SeaAnimal a : jurassicSeaAnimals) {
+            System.out.println("A " + a.getName() + " " + a.getSwimming());
+        }
+        System.out.println("===== " + jurassic.getEra() + " sky animals section =====");
+        List<SkyAnimal> jurassicSkyAnimals = jurassic.createSkyAnimals();
+        for (SkyAnimal a : jurassicSkyAnimals) {
+            System.out.println("A " + a.getName() + " " + a.getFlying());
+        }
+        System.out.println("Thank you for visiting the " + jurassic.getEra() + " exhibition");
     }
 }
