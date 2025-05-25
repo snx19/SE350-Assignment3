6ced6bc7480f0d3865fee407d98bfe02b501cf07
snx19 <hayato@msnx.local>
2025-05-24 22:35:57 -0500

    Added CenozoicAnimals.java

---
+++ src/main/java/Museum/CenozoicAnimals.java
@@ -0,0 +1,99 @@
+package Museum;
+
+/**
+ * class Mammoth
+ */
+class Mammoth implements LandAnimal {
+    /**
+     * gets name
+     * @return Mammoth
+     */
+    public String getName() {return "Mammoth";}
+    /**
+     * gets walking
+     * @return stomping
+     */
+    public String getWalking() {return "stomping";}
+}
+
+/**
+ * class CaveLion
+ */
+class CaveLion implements LandAnimal {
+    /**
+     * gets name
+     * @return Cave lion
+     */
+    public String getName() {return "Cave lion";}
+    /**
+     * gets walking
+     * @return walking
+     */
+    public String getWalking() {return "walking";}
+}
+
+/**
+ * class Woolly Rhino
+ */
+class WoollyRhino implements LandAnimal {
+    /**
+     * gets name
+     * @return Woolly rino
+     */
+    public String getName() {return "Woolly rhino";}
+    /**
+     * gets walking
+     * @return running
+     */
+    public String getWalking() {return "running";}
+}
+
+/**
+ * class Otodus
+ */
+class Otodus implements SeaAnimal {
+    /**
+     * gets name
+     * @return name
+     */
+    public String getName() {return "Otodus";}
+    /**
+     * gets swimming
+     * @return hunting
+     */
+    public String getSwimming() {return "hunting";}
+}
+
+/**
+ * class whale
+ */
+class Whale implements SeaAnimal {
+    /**
+     * gets name
+     * @return Whale
+     */
+    public String getName() {return "Whale";}
+    /**
+     * gets swimming
+     * @return swimming
+     */
+    public String getSwimming() {return "swimming";}
+}
+
+/**
+ * class Argentavis
+ */
+class Argentavis implements SkyAnimal {
+    /**
+     * gets name
+     * @return Argentavis
+     */
+    public String getName() {return "Argentavis";}
+
+    /**
+     * gets flying
+     * @return flapping
+     */
+    public String getFlying() {return "flapping";}
+}
+
