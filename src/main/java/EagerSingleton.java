3ec8ec23a0a2e1367018a23cf7cb0b4da056b35c
snx19 <hayato@msnx.local>
2025-05-24 21:15:18 -0500

    aded code/javadoc for EagerSingleton.java

--- src/main/java/EagerSingleton.java
+++ src/main/java/EagerSingleton.java
@@ -1,2 +1,39 @@
+/**
+ * EagerSingleton creates 3 instances at load time
+ */
 public class EagerSingleton {
+    private static final EagerSingleton[] instance = new EagerSingleton[3];
+    private int id;
+
+    /**
+     * private constructor
+     * @param id the id the instance
+      */
+    private EagerSingleton(int id) {
+        this.id = id;
+    }
+
+    // static initializer runs when class loaded
+    static {
+        for (int i = 0; i < 3; i++) { // creates 3 instances
+            instance[i] = new EagerSingleton(i);
+            System.out.println("EagerSingleton " + i + " instantiated");
+        }
+    }
+
+    /**
+     * Returns pre-created instance
+     * @param index the index of instance
+     * @return instance[index] instance at specified index
+     */
+    public static EagerSingleton getInstance(int index) {
+        return instance[index];
+    }
+    /**
+     * returns id of EagerSingleton instance
+     * @return id 0-3
+      */
+    public int getId() {
+        return id;
+    }
 }
