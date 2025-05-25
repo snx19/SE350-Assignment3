4398440df84ee23da1b3719366e5f579c64136ee
snx19 <hayato@msnx.local>
2025-05-24 23:22:00 -0500

    added @override

--- src/main/java/Museum/CenozoicAnimals.java
+++ src/main/java/Museum/CenozoicAnimals.java
@@ -8,11 +8,13 @@ class Mammoth implements LandAnimal {
      * gets name
      * @return Mammoth
      */
+    @Override
     public String getName() {return "Mammoth";}
     /**
      * gets walking
      * @return stomping
      */
+    @Override
     public String getWalking() {return "stomping";}
 }
 
@@ -24,11 +26,13 @@ class CaveLion implements LandAnimal {
      * gets name
      * @return Cave lion
      */
+    @Override
     public String getName() {return "Cave lion";}
     /**
      * gets walking
      * @return walking
      */
+    @Override
     public String getWalking() {return "walking";}
 }
 
@@ -40,11 +44,13 @@ class WoollyRhino implements LandAnimal {
      * gets name
      * @return Woolly rino
      */
+    @Override
     public String getName() {return "Woolly rhino";}
     /**
      * gets walking
      * @return running
      */
+    @Override
     public String getWalking() {return "running";}
 }
 
@@ -56,11 +62,13 @@ class Otodus implements SeaAnimal {
      * gets name
      * @return name
      */
+    @Override
     public String getName() {return "Otodus";}
     /**
      * gets swimming
      * @return hunting
      */
+    @Override
     public String getSwimming() {return "hunting";}
 }
 
@@ -72,11 +80,13 @@ class Whale implements SeaAnimal {
      * gets name
      * @return Whale
      */
+    @Override
     public String getName() {return "Whale";}
     /**
      * gets swimming
      * @return swimming
      */
+    @Override
     public String getSwimming() {return "swimming";}
 }
 
@@ -88,12 +98,14 @@ class Argentavis implements SkyAnimal {
      * gets name
      * @return Argentavis
      */
+    @Override
     public String getName() {return "Argentavis";}
 
     /**
      * gets flying
      * @return flapping
      */
+    @Override
     public String getFlying() {return "flapping";}
 }
 
