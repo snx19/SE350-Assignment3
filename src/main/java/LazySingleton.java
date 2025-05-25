e83759085f4c53fc52f4d0fc1716cff193b6c881
snx19 <hayato@msnx.local>
2025-05-24 21:20:00 -0500

    added code/javadoc for LazySingleton.java

--- src/main/java/LazySingleton.java
+++ src/main/java/LazySingleton.java
@@ -1,2 +1,37 @@
+/**
+ * LazySingleton creates up to 3 instances when requested
+ */
 public class LazySingleton {
+    private static LazySingleton[] instance = new LazySingleton[3];
+    private int id;
+
+    /**
+     * private constructor
+     * @param id the id of the instance
+     */
+    private LazySingleton(int id) {
+        this.id = id;
+        System.out.println("Lazy Singleton " + id + " instantiated");
+    }
+
+    /**
+     * returns LazySingleton instance at index
+     * creates if not created already
+     * @param index the index of the instance
+     * @return instance index
+     */
+    public static LazySingleton getInstance(int index) {
+        if (instance[index] == null) {
+            instance[index] = new LazySingleton(index);
+        }
+        return instance[index];
+    }
+
+    /**
+     * return the id of LazySingleton instance
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
 }
